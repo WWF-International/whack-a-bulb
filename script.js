17b635a874d7e2073f4360d1ef129def43996d6e
Ash Clarke <aclarke@wwf.org.uk>
2015-03-27 15:00:23 +0000

    text changes to make it 'whack a bulb'

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     var offset = $('#clickspace').offset();
     var squares = [];
     var timeout;
-    var twitterMsg="I scored {{SCORE}} playing smack-a-bulb on #EarthHourUK's website. Can you beat my score?";
+    var twitterMsg="I scored {{SCORE}} playing whack-a-bulb on #EarthHourUK's website. Can you beat my score?";
     var linkUrl = window.top.location.href;
     var originalUrl = window.top.location.href;
 
